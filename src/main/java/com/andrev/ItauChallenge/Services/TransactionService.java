18f951754c1352af9cbf23f66763d5e97d718848
frogzinkk <andre.liborio2005@gmail.com>
2025-10-28 17:03:50 -0300

    feat:create TransactionService

--- src/main/java/com/andrev/ItauChallenge/Services/TransactionService.java
+++ src/main/java/com/andrev/ItauChallenge/Services/TransactionService.java
@@ -1,4 +1,23 @@
 package com.andrev.ItauChallenge.Services;
 
+import com.andrev.ItauChallenge.Models.TransactionModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@Service
 public class TransactionService {
+
+  TransactionModel transactionModel;
+  Queue<TransactionModel> transactionModelQueue = new LinkedList<>();
+
+  public TransactionModel transaction(TransactionModel transactionModel){
+    transactionModelQueue.add(transactionModel);
+    return transactionModel;
+  }
+
+
 }
